f50347b6629a8886170806faca01f40c6ac7ec4a
Darshaka Pathirana <dpat@syn-net.org>
2024-11-17 13:23:31 +0100

    Allow linux-desktop-bootstrap.sh to be run as root without sudo
    On systems where there is no admin user with sudo rights, it may be
    useful to install the base programs without sudo.

--- linux-desktop-bootstrap.sh
+++ linux-desktop-bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Filename:      linux-desktop-boostrap.sh
 # Purpose:       Bootstrap a Linux Desktop
 # Authors:       Darshaka Pathirana <dpat@syn-net.org>
@@ -6,8 +6,23 @@
 # License:       This file is licensed under the GPL v2.
 ################################################################################
 
-sudo apt update -q
-sudo apt install -q -y --no-install-recommends git etckeeper ansible-core
+set -euo pipefail
+
+if id -u | grep -q "^0"; then
+  APT_COMMAND="apt"
+else
+  if command -v sudo &> /dev/null; then
+    APT_COMMAND="sudo apt"
+  else
+    echo "Error: Not running as root and sudo is not available."
+    echo "Please run this script with sudo or as root."
+    exit 1
+  fi
+fi
+
+${APT_COMMAND} update -q
+${APT_COMMAND} install -q -y --no-install-recommends git etckeeper ansible-core
+
 [ ! -d "${HOME}/.dotfiles" ] && git clone https://github.com/jkirk/dotfiles.git "${HOME}/.dotfiles"
 [ -d "${HOME}/.dotfiles" ] && ( cd "${HOME}/.dotfiles"; git pull; sh "./create-symlinks.sh" ) || exit 1
 [ ! -d "${HOME}/linux-desktop-bootstrap" ] && git clone https://github.com/jkirk/linux-desktop-bootstrap
